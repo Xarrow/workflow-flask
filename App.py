c4af1a27a0baa0d3cd3b3daedc574546ff4d9a74
helixcs <zhangjian12424@gmail.com>
2023-10-22 15:51:10 +0000

    init

--- App.py
+++ App.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask import request
+
+from singlefile import SingleFile
 
 app = Flask(__name__)
+singleFile = SingleFile(chrome_cwd="/usr/bin/google-chrome-stable", single_file_cwd="/root/single-file-cli")
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/singleFile", methods=['GET'])
+def single_file():
+    url = request.args.get("url")
+    return singleFile.execute(url)
 
 
 if __name__ == '__main__':
