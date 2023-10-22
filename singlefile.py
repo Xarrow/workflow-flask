a0faca3ba6b4ec4ec06deab96407fc4ebe73b6fc
helixcs <zhangjian12424@gmail.com>
2023-10-22 15:37:49 +0000

    init

---
+++ singlefile.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import subprocess
+
+
+class SingleFile(object):
+    def __init__(self, chrome_cwd, single_file_cwd):
+        self.chrome_cwd = chrome_cwd
+        self.single_file_cwd = single_file_cwd
+
+    def prepare_env(self):
+        #  1. node  env
+        #  2. chrome env
+        pass
+
+    def execute(self, url: str) -> str:
+        command = f'''node single-file --browser-executable-path {self.chrome_cwd} {url} --back-end=puppeteer --dump-content'''
+        print(command)
+        args = command.split(" ")
+        args = list(filter(lambda x: x is not None and x != '' and x != '\n', args))
+        completed_process = subprocess.run(args,
+                                           shell=False,
+                                           capture_output=True,
+                                           cwd=self.single_file_cwd,
+                                           encoding="utf-8")
+        return str(completed_process.stdout)
+
+
+if __name__ == '__main__':
+    single_file = SingleFile(chrome_cwd="/usr/bin/google-chrome-stable", single_file_cwd="/root/single-file-cli")
+    print(single_file.execute("https://baidu.com"))
